55af6345f7b81dac56761ac8eb8cd567b415bf82
Kendel Chopp <conact@kchopp.com>
2017-07-21 13:02:33 -0500

    Initial Commit

---
+++ ChoppHandwritingRecognizer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ChoppHandwritingRecognizer
+//
+//  Created by Kendel Chopp on 7/21/17.
+//  Copyright © 2017 Kendel Chopp. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
